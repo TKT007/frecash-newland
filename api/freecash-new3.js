8356cc88fc486d1d1975f6daf4c24cf0e62941c2
TKT007 <makosquad.ze@gmail.com>
2025-12-11 02:44:46 -0300

    Update freecash-new3.js

--- api/freecash-new3.js
+++ api/freecash-new3.js
@@ -1,8 +1,8 @@
 const axios = require('axios');
 
 module.exports = async (req, res) => {
-  const TIKTOK_PIXEL_ID = process.env.TIKTOK_PIXEL_ID_FREECASH_NEW || 'D4S5A6JC77UE9IMLMC0G';
-  const TIKTOK_ACCESS_TOKEN = process.env.TIKTOK_ACCESS_TOKEN_FREECASH_NEW;
+  const TIKTOK_PIXEL_ID = process.env.TIKTOK_PIXEL_ID_ZELLE || 'D41PHSBC77UA61AHK3NG';
+  const TIKTOK_ACCESS_TOKEN = process.env.TIKTOK_ACCESS_TOKEN_ZELLE;
   
   // CORS - IMPORTANTE!
   res.setHeader('Access-Control-Allow-Origin', '*');
